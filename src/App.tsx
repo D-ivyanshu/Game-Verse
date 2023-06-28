585887dc802d12c300bc5fcf3dc4322f64233c1f
Divyanshu <divyanshu.20224@knit.ac.in>
2023-06-28 23:40:12 +0530

    Build Responsive Laout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from "react";
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
     <>
-      <Button colorScheme='blue' size='lg'>
-        Button
-      </Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main" `,
+        }}
+      >
+        <GridItem bg="red" area={"nav"}>
+          Nav
+        </GridItem>
+        <Show above="lg">
+        <GridItem bg="yellow" area={"aside"}>
+          aside
+        </GridItem>
+        </Show>
+        <GridItem bg="green" area={"main"}>
+          Main
+        </GridItem>
+      </Grid>
     </>
-  )
+  );
 };
 
 export default App;
