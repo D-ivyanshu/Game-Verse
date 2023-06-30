b07bb62a014d2401aa35e28d7ede65aa208412c4
Divyanshu <divyanshu.20224@knit.ac.in>
2023-06-30 13:42:19 +0530

    Instead of calling genre from api we store that in genre.js

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -5,7 +5,7 @@ const GameCardSkeleton = () => {
   return (
     // <Card width={'340px'} borderRadius={10} overflow='hidden'> 
      <Card width="100%" borderRadius={10} overflow="hidden"> 
-        <Skeleton height={'200px'} />
+        <Skeleton height={'250px'} />
         <CardBody>
             <SkeletonText />
         </CardBody>
