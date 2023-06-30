b5a1de2192853575ea1f30a213cc1d1ed9d1973b
Divyanshu <divyanshu.20224@knit.ac.in>
2023-06-30 13:18:43 +0530

    Implemented Searching

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 
 const App = () => {
@@ -35,7 +36,7 @@ const App = () => {
         }}
       >
         <GridItem area={"nav"}>
-          <Navbar />
+          <Navbar onSearch={(searchText) => setGameQuery({...gameQuery, searchText})}/>
         </GridItem>
         <Show above="lg">
         <GridItem area="aside" paddingX={5}>
